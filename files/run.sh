4775e70a2ead6561493c6a123b0bac6318e841bb
Gustav Wulf <gustav@neer.se>
2024-11-25 09:45:10 +0100

    Added missing schema for debug.

--- files/run.sh
+++ files/run.sh
@@ -20,7 +20,7 @@ CAM12_SN=$(jq --raw-output ".camera_12_serial_number" $CONFIG_PATH)
 CAM13_SN=$(jq --raw-output ".camera_13_serial_number" $CONFIG_PATH)
 CAM14_SN=$(jq --raw-output ".camera_14_serial_number" $CONFIG_PATH)
 CAM15_SN=$(jq --raw-output ".camera_15_serial_number" $CONFIG_PATH)
-DEBUG=$(jq --raw-output ".debug" $CONFIG_PATH)
+DEBUG=$(jq --raw-output ".debug_log" $CONFIG_PATH)
 
 echo "Starting EufyP2PStream. eufy_security_ws_port is $EUFY_WS_PORT"
 python3 -u /eufyp2pstream.py --debug $DEBUG --ws_security_port $EUFY_WS_PORT --camera_serials $CAM1_SN $CAM2_SN $CAM3_SN $CAM4_SN $CAM5_SN $CAM6_SN $CAM7_SN $CAM8_SN $CAM9_SN $CAM10_SN $CAM11_SN $CAM12_SN $CAM13_SN $CAM14_SN $CAM15_SN
